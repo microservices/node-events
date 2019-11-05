fee5217cb7985528435f3ea2d18225cc87ad91e8
StoryscriptHuman <53434738+StoryscriptHuman@users.noreply.github.com>
2019-11-06 00:21:55 +0400

    ci: dd support for OMS_PATH in node-events (#6)

--- test/runner.js
+++ test/runner.js
@@ -17,16 +17,19 @@ const payloads = JSON.parse(readFileSync(payloadFile));
 // get additional CLI arguments from the payload file name
 const actions = basename(payloadFile).split('.')[0].split('_');
 
+const OMS_PATH = process.env.OMS_PATH || null
+
 function test(cb) {
-  const args = ['oms', 'subscribe', '--silent', 'listen', ...actions];
+  let oms
+  const args = ['subscribe', '--silent', 'listen', ...actions];
   console.log(`Spawn: ${args.join(' ')}`);
-  const oms = spawn('npx', args);
-  oms.stdout.on('data', (data) => {
-    process.stdout.write(data);
-  });
-  oms.stderr.on('data', (data) => {
-    process.stdout.write(data);
-  });
+  if (OMS_PATH) {
+    oms = spawn(OMS_PATH, args)
+  } else {
+    oms = spawn('npx', ['oms'].concat(args))
+  }
+  oms.stdout.pipe(process.stdout)
+  oms.stderr.pipe(process.stderr)
   cb(oms);
 }
 
